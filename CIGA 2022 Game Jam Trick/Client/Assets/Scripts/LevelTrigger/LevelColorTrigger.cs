e3fbc8f5a2be7e5146dd06ebfc586683bd4104dc
gjbluestar <290160719@qq.com>
2022-06-25 15:04:33 +0800

    Color change function

---
+++ CIGA 2022 Game Jam Trick/Client/Assets/Scripts/LevelTrigger/LevelColorTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelColorTrigger : MonoBehaviour
+{
+    GameObject m_BackGround;
+
+    private void Start()
+    {
+        m_BackGround = GameObject.Find("PureBackGround");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null && other.transform != null)
+        {
+            MainRole mainRole = other.transform.GetComponent<MainRole>();
+            if (mainRole)
+            {
+                if (m_BackGround)
+                {
+                    MeshRenderer thisMesh = GetComponent<MeshRenderer>();
+                    if (thisMesh != null)
+                    {
+                        Color color = thisMesh.sharedMaterial.GetColor("_Color");
+                        MeshRenderer mesh = m_BackGround.GetComponent<MeshRenderer>();
+                        if (mesh != null)
+                        {
+                            mesh.material.SetColor("_Color", color);
+                            GameManager.GetInstance().CurrentPureBGColor = color;
+                            GameObject.Destroy(gameObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
