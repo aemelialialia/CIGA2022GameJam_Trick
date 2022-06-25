f765d928830b4b542f5b15e2039d4741d2a7c558
gjbluestar <290160719@qq.com>
2022-06-25 12:15:17 +0800

    modify conflict

--- CIGA 2022 Game Jam Trick/Client/Assets/Scripts/MainRole.cs
+++ CIGA 2022 Game Jam Trick/Client/Assets/Scripts/MainRole.cs
@@ -9,17 +9,20 @@ public enum ECharacterState
 }
 public class MainRole : MonoBehaviour
 {
-    public ECharacterState m_State;
-    private Rigidbody m_Rigidbody;
-    private Transform CacheTransform;
+    #region Public
     public float Speed = 5;
     public int m_PlayerId = 0;
-    public float JumpForce = 5;
+    public float JumpForce = 1;
+    public float JumpInterval = 1;
+    #endregion
+
+    private ECharacterState m_State;
+    private Rigidbody m_Rigidbody;
+    private Transform CacheTransform;
     private InputManager Input;
-    public float m_MoveX; // Variable to store the horizontal Input each frame
-    public float m_MoveY; // Variable to store the vectical Input each frame
-    private int GroundLayer;
-    private float m_RayLength = 1;
+
+
+    private bool m_ReverseJump;
     // Start is called before the first frame update
     
     // force to right, moveX ++
@@ -30,51 +33,39 @@ public class MainRole : MonoBehaviour
         Input = InputManager.GetInstance();
         CacheTransform = transform;
         m_Rigidbody = GetComponent<Rigidbody>();
-        GroundLayer = LayerMask.NameToLayer("Ground");
+        m_ReverseJump = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Update the moveX Variable and assign the current vertical input for this frame
-        
-        //m_MoveX = (int)Input.GetAxis(m_PlayerId, InputAction.MoveX);
-        m_MoveX = Input.GetAxis(m_PlayerId, InputAction.MoveX);
-        m_MoveX = (float)(m_MoveY + (RightForce * 0.1));
-
-        // Update the moveY Variable and assign the current vertical input for this frame
-        m_MoveY = (int)Input.GetAxis(m_PlayerId, InputAction.MoveY);
         bool Jump = Input.GetButtonDown(m_PlayerId, InputAction.Jump);
         if (Jump && m_State == ECharacterState.Idle)
         {
-            m_Rigidbody.AddForce(new Vector3(0, JumpForce, 0));
+            m_Rigidbody.AddForce(new Vector3(0, m_ReverseJump? -JumpForce: JumpForce, 0));
+            m_Rigidbody.velocity = Vector3.zero;
             m_State = ECharacterState.Jump;
-            StartCoroutine(GroundRayCastCheck());
+            StartCoroutine(JumpToIdle());
         }
         UpdatePhysics();
 
-        Debug.DrawLine(transform.position, transform.position + new Vector3(0, -m_RayLength, 0),Color.red);
+        //Debug.DrawLine(transform.position, transform.position + new Vector3(0, -m_RayLength, 0),Color.red);
     }
 
-    IEnumerator GroundRayCastCheck()
+    public void SetReverseJump(bool active)
     {
-        yield return new WaitForSeconds(1);
-        while (m_State == ECharacterState.Jump)
-        {
-            if (Physics.Raycast(transform.position, new Vector3(0, -1, 0), m_RayLength))
-            {
-                m_State = ECharacterState.Idle;
-            }
-            yield return 0;
-        }
+        m_ReverseJump = active;
     }
 
-
-
+    IEnumerator JumpToIdle()
+    {
+        yield return new WaitForSeconds(JumpInterval);
+        m_State = ECharacterState.Idle;
+    }
 
     void UpdatePhysics()
     {
-        Vector3 move = new Vector3(m_MoveX,0, m_MoveY) * Speed * Time.deltaTime;
+        Vector3 move = Vector3.right * Speed * Time.deltaTime;
         CacheTransform.position = CacheTransform.position + move;
     }
 }
