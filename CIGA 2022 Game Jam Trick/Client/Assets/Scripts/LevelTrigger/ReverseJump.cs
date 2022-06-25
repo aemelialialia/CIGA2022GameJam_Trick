c3e129a0e247c41774586a588c9ebb4b4029cf21
gjbluestar <290160719@qq.com>
2022-06-25 12:00:58 +0800

    Jump and reverse Jump

---
+++ CIGA 2022 Game Jam Trick/Client/Assets/Scripts/LevelTrigger/ReverseJump.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseJump : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null && other.transform != null)
+        {
+            MainRole role = other.transform.GetComponent<MainRole>();
+            if (role)
+            {
+                role.SetReverseJump(true);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != null && other.transform != null)
+        {
+            MainRole role = other.transform.GetComponent<MainRole>();
+            if (role)
+            {
+                role.SetReverseJump(false);
+            }
+        }
+    }
+}
