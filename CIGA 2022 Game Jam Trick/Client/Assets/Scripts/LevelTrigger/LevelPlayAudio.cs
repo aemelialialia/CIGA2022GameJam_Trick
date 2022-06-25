6443968ec515b623a5353ed3de7621f56b581c3e
gjbluestar <290160719@qq.com>
2022-06-25 20:52:12 +0800

    audio

---
+++ CIGA 2022 Game Jam Trick/Client/Assets/Scripts/LevelTrigger/LevelPlayAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPlayAudio : MonoBehaviour
+{
+    public AudioClip AudioClip;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null && other.transform != null)
+        {
+            MainRole role = other.transform.GetComponent<MainRole>();
+            if (role && AudioClip)
+            {
+                AudioManager.GetInstance().PlayCilp(AudioClip);
+            }
+        }
+    }
+}
